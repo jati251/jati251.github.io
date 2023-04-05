2a28a622a91b6177f9da9c6f6b25483196810f3c
Jati Suryo <jati.suryo@gmail.com>
2023-04-05 23:02:12 +0700

    update

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-let nama = ''
+  let nama = ''
+let kosong= document.getElementById('kosong')
 
-var pageName = document.getElementById('page-name')
-var intro1 = document.getElementById('intro1')
 
 document.getElementById('myButton').onclick = function () {
   nama = document.getElementById('myText').value;
-  document.getElementById('introcon').style.display = 'none';
-  intro1.style.display= 'inline'
-  pageName.innerHTML = `Haloo ${nama} !`;
+  if(nama===''||!nama){
+    kosong.style.display= 'inline';
+  } else {
+    location.href = "./homepage.html";
+  }
 };
 
-document.getElementById('button-back').onclick = function (){
-  document.getElementById('introcon').style.display = 'inline';
-  intro1.style.display= 'none'
-}
+let baru=nama
 
+export {baru}
 
 
