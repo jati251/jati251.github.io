bb76680bbe256836c5d1d5be7140545ae05fcc0e
Jati Suryo <jati.suryo@gmail.com>
2023-04-05 19:38:47 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-document.getElementById('myButton').onclick= function(){
-  var namaDepan=document.getElementById('fname').value;
-  var namaBelakang=document.getElementById('lname').value;
-  location.href = "www.youtube.com";
-}
+let nama = ''
+
+var pageName = document.getElementById('page-name')
+var intro1 = document.getElementById('intro1')
+
+document.getElementById('myButton').onclick = function () {
+  nama = document.getElementById('myText').value;
+  document.getElementById('introcon').style.display = 'none';
+  intro1.style.display= 'inline'
+  pageName.innerHTML = `Haloo ${nama} !`;
+};
+
+
