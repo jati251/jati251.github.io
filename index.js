28251bc0d87c58e61e38556bbdca891256dc32a9
Jati Suryo <jati.suryo@gmail.com>
2023-04-06 00:48:53 +0700

    update

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-  let nama = ''
-let kosong= document.getElementById('kosong')
 
 
-document.getElementById('myButton').onclick = function () {
-  nama = document.getElementById('myText').value;
-  if(nama===''||!nama){
-    kosong.style.display= 'inline';
-  } else {
-    location.href = "./homepage.html";
-  }
-};
-
-let baru=nama
-
-export {baru}
 
 
